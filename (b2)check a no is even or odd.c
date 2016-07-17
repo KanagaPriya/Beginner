965c99dbbcc7f84e562ce7b28902046b2da201f9
KanagaPriya <kanagapriyakpr@gmail.com>
2016-07-17 06:06:59 -0700

    Create (b2)check a no is even or odd.c

---
+++ (b2)check a no is even or odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter the value of n");
+scanf("%d",&n);
+if(n%2==0)
+printf("\n the number is even");
+else
+printf("\n the number is odd");
+getch();
+}
